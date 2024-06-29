4d928e70cf0ed30047290c88574e853575f2cea9
temitope david dairo <dairotemitopedavid@gmail.com>
2024-06-29 16:22:37 +0100

    Update settings.py

--- shallyproject/settings.py
+++ shallyproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_or_create_secret_key()
+SECRET_KEY = 'django-insecure-rx19=!&r@@n2a@mtw(3@+yf=n=&+8pd3gk-2tm_$n2i89it#3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
